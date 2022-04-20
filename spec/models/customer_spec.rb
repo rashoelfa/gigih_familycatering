6537d898bd819ea390838ed770aa09528c22fe4d
rashoelfa <rashoelfa@gmail.com>
2022-04-20 05:34:22 +0000

    added customer test and factory

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'has a valid factory' do
+    expect(FactoryBot.build(:customer)).to be_valid
+  end
+
+  it 'is invalid with not email formatted' do
+    customer = FactoryBot.build(:customer, email: "asdasdasdasdasd")
+    customer.valid?
+    expect(customer.errors[:email]).to include("must insert email format")
+  end
 end
