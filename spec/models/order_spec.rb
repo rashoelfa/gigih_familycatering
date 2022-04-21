e738745f7808115c48271aec348a303c647a4718
rashoelfa <rashoelfa@gmail.com>
2022-04-21 20:27:00 +0000

    added test on order model

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,56 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'self#show_customer_order' do
+    it 'should return a array of results that match' do
+      Category.create(
+        name: 'Makanan'
+      )
+      Category.create(
+        name: 'Minuman'
+      )
+
+      Customer.create(
+        name: 'Mary Jones',
+        email: 'maryjones@gmail.com'
+      )
+
+      MenuItem.create(
+        name: 'Cordon Bleu',
+        price: 26000.0
+      )
+      MenuItem.create(
+        name: 'Spaghetti',
+        price: 30000.0
+      )
+
+      ItemCategory.create(
+        menu_item_id: 1,
+        category_id: 1
+      )
+
+      Order.create(
+        customer_id: 1,
+        total: 56000.0,
+        order_date: '2020-10-10',
+        status: 'NEW'
+      )
+
+      OrderDetail.create(
+        order_id: 1,
+        menu_item_id: 1,
+        quantity: 1,
+      )
+
+      OrderDetail.create(
+        order_id: 1,
+        menu_item_id: 2,
+        quantity: 1,
+      )
+
+      result = { 'order_id' => 1, 'order_date' => '2020-10-10', 'name' => 'Mary Jones', 'email' => 'maryjones@gmail.com', 'total' => 56000.0, 'items' => 'Cordon Bleu,Spaghetti', 'status' => 'NEW' }
+
+      expect(Order.show_customer_order).to include(result)
+    end
+  end
 end
