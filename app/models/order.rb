9e3add2d7449dcfe416f7b2ea0a1bfd5d02f6531
rashoelfa <rashoelfa@gmail.com>
2022-04-21 20:27:43 +0000

    fix relationshop between models and added validation for order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,14 @@
 class Order < ApplicationRecord
+  has_many :order_details
+  
+  def self.show_customer_order
+    query = "select order_details.order_id, orders.order_date, customers.name, customers.email, orders.total, GROUP_CONCAT(menu_items.name) as items, orders.status
+from orders
+join order_details on orders.id = order_details.order_id
+join customers on orders.customer_id = customers.id
+join menu_items on order_details.menu_item_id = menu_items.id
+group by order_details.order_id;"
+    results = ActiveRecord::Base.connection.execute(query)
+    return results
+  end
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,2 +1,4 @@
 class OrderDetail < ApplicationRecord
+  belongs_to :order
+  belongs_to :menu_item
 end
