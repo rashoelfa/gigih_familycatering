a1b12f27356ab0d984d7bc1ee71de64e4fc75ac6
rashoelfa <rashoelfa@gmail.com>
2022-04-22 18:41:28 +0000

    added spec that necessary

---
+++ spec/requests/menu_spec.rb
@@ -0,0 +1,39 @@
+# require 'rails_helper'
+
+# RSpec.describe 'Menus', type: :request do
+#   describe 'GET /menu' do
+#     context 'with params[:category]' do
+#       it 'renders the json' do
+#         actual = response.body
+#         expected = { foo: 'bar' }.to_json
+#         expect(actual).to eq expected
+#       end
+#     end
+
+#     context 'without params[:letter]' do
+#       it 'renders the json' do
+#         expect(response).to have_http_status(:ok)
+#       end
+#     end
+#   end
+
+#   describe 'GET /category' do
+#     pending "add some examples (or delete) #{__FILE__}"
+#   end
+
+#   describe 'GET /menu/create' do
+#     pending "add some examples (or delete) #{__FILE__}"
+#   end
+
+#   describe 'GET /menu/create/category' do
+#     pending "add some examples (or delete) #{__FILE__}"
+#   end
+
+#   describe 'GET /menu/update' do
+#     pending "add some examples (or delete) #{__FILE__}"
+#   end
+
+#   describe 'GET /menu/delete' do
+#     pending "add some examples (or delete) #{__FILE__}"
+#   end
+# end
--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,3 +1,10 @@
+module ApiHelpers
+  def json_body
+    JSON.parse(response.body)
+  end
+end
+
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
+  config.include ApiHelpers, type: :request
 end
